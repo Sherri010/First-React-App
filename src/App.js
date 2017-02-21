b5be6a27089c2784ce1e40d24175f75a2cda1ef1
sherri010 <aminshahidi.sh@gmail.com>
2017-02-20 18:52:59 -0800

    adds add project component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 import Projects from './components/project'
+import AddProject from './components/addproject.js'
 import './App.css';
 
 class App extends Component {
   constructor(){
       super();
     this.state = {
-      projects :[{
-        title:"My portfolio",
-        deadline:" March 2016"
-      },{
-        title:"Event Planner",
-        deadline:"April"
-      },{
-        title:"Airplanner",
-        deadline:"July"
-      }
-    ]
+      project:[]
     }
-  }
+ }
+
+
+  componentWillMount(){
+        this.setState({
+        projects :[{
+          title:"My portfolio",
+          deadline:" March 2016"
+        },{
+          title:"Event Planner",
+          deadline:"April"
+        },{
+          title:"Airplanner",
+          deadline:"July"
+        }
+       ]})
+    }
+
   render() {
     return (
       <div className="App">
         Project Management
         <Projects projects={this.state.projects}/>
+        <AddProject />
       </div>
     );
   }
---
+++ src/components/addproject.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    return (
+      <h3>
+      Add project
+      </h3>
+    );
+  }
+}
+
+export default Projects;
