36a7b56a7e4b3e0c84c16c6ca310e00bbafd355b
sherri010 <aminshahidi.sh@gmail.com>
2017-02-21 19:02:56 -0800

    adds add project from

--- src/components/addproject.js
+++ src/components/addproject.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 
-class Projects extends Component {
-  render() {
-    return (
-      <h3>
-      Add project
-      </h3>
-    );
+class AddProject extends Component {
+  constructor(){
+    super();
+    this.satate={
+      newProject:[]
+    }
   }
+  static defaultProps ={
+    categories:["web","mobile","design"]
+  }
+
+  handleSubmit(e){
+       e.preventDefault();
+    console.log(this.refs.title.value);
+
+  }
+
+  render() {
+    let cats = this.props.categories.map(category =>{
+       return <option key={category} value={category}>{category}</option>
+     });
+    return (
+    <div>
+      <h3> Add new project </h3>
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <div>
+          <label>Title</label> <br />
+          <input type="text" ref="title" />
+        </div>
+        <div>
+          <label>Cat</label> <br />
+          <select ref="category">
+             {cats}
+          </select>
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+    )}
 }
 
-export default Projects;
+export default AddProject;
